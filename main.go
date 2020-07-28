5d157292805122af9790726ac07001dd031d8209
DuongVu101498 <48491289+DuongVu101498@users.noreply.github.com>
2020-07-28 11:04:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 
      http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path)
+        http.ServeFile(w, r, html.EscapeString(r.URL.Path))
     })
 
+    http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hi")
     })
